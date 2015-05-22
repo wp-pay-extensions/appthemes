597d8a7a9da228be0662a44904482f5df79955ca
Remco Tolsma <info@remcotolsma.nl>
2015-05-22 20:30:49 +0200

    Updated doc block copyright year to 2015.

--- src/PaymentData.php
+++ src/PaymentData.php
@@ -3,10 +3,10 @@
 /**
  * Title: WordPress AppThemes payment data
  * Description:
- * Copyright: Copyright (c) 2005 - 2011
+ * Copyright: Copyright (c) 2005 - 2015
  * Company: Pronamic
  * @author Remco Tolsma
- * @version 1.0
+ * @version 1.0.0
  */
 class Pronamic_WP_Pay_Extensions_AppThemes_PaymentData extends Pronamic_WP_Pay_PaymentData {
 	/**
