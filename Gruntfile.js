496a0650f3d1e111f2f3d51a24ef306bbcbf2155
Reüel van der Steege <reuel@pronamic.nl>
2018-03-06 11:29:41 +0100

    Rename phpcs.ruleset.xml to phpcs.xml.dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function( grunt ) {
 			},
 			options: {
 				bin: 'vendor/bin/phpcs',
-				standard: 'phpcs.ruleset.xml',
+				standard: 'phpcs.xml.dist',
 				showSniffCodes: true
 			}
 		},
